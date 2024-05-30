2526b20348c860dccfece714556050968d1878ac
Mia Quinn <mia.quinn@tipalti.com>
2024-05-30 15:13:04 +0100

    feat: added hover effect over buttons

--- src/components/BoxComponent.tsx
+++ src/components/BoxComponent.tsx
@@ -10,7 +10,7 @@ type BoxComponentProps = {
 const BoxComponent: React.FC<BoxComponentProps>= ({data, units, colour}) => {
     // console.log(colour);
     return (
-        <div className="bg-white shadow rounded-lg flex flex-col items-center">
+        <div className="bg-white shadow rounded-lg flex flex-col items-center hover:bg-gray-50">
             <div className="text-4xl text-gray-700 font-bold pt-5">{data}</div>
             <div id="small-text" className="p-2" >{units}</div>
             <div style={{backgroundColor:colour}} className="bg-teal-400 rounded-b-lg p-1 flex-1 items-end w-full"></div>
